6dc7f21fc05e060229d223eda4c37ea5604df3db
AniketYadav17 <anikety0000@gmail.com>
2021-12-31 23:06:43 +0530

    third commit

--- Demo/settings.py
+++ Demo/settings.py
@@ -128,8 +128,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 #STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
+#STATIC_ROOT = BASE_DIR / "static"
 
 # STATICFILES_DIR = {
 #     os.path.join(BASE_DIR , "static")
